838cd304eba279df954ed12aad4495c62bf3d7e8
Arkshine <hv.contact@gmail.com>
2015-04-04 16:01:43 +0200

    Add licence headers

--- module/sw_detours.h
+++ module/sw_detours.h
@@ -1,3 +1,10 @@
+//
+// AMX Mod X SteamTools
+// Copyright (C) Vincent Herbet (Arkshine)
+//
+// This software is licensed under the GNU General Public License, version 2.
+// For full license details, see LICENSE file.
+//
 
 #ifndef _SW_DETOURS_H_
 #define _SW_DETOURS_H_
--- module/utils.h
+++ module/utils.h
@@ -1,3 +1,10 @@
+//
+// AMX Mod X SteamTools
+// Copyright (C) Vincent Herbet (Arkshine)
+//
+// This software is licensed under the GNU General Public License, version 2.
+// For full license details, see LICENSE file.
+//
 
 #ifndef _UTILS_H_
 #define _UTILS_H_
